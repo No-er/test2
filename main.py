a8866afa3b96a9f966b451360e6c0aeed26d66a7
No-er <goglmogl.com@gmail.com>
2023-10-22 14:52:27 +0700

    asuncio aiohttp lesson 7 part 2

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 import asyncio
+import aiohttp
 from time import time
+import random
 
-async def first():
-    num = 0
-    while True:
-        print(num)
-        num += 1
-        await asyncio.sleep(1)
 
-async def second():
-    while True:
-        print(time())
-        await asyncio.sleep(1)
+def write_files(file):
+    file_name = random.randint(0, 100000)
+    with open(f'{file_name}', 'wb') as f:
+        f.write(file)
 
-async def loop():
-    task1 = asyncio.create_task(first())
-    task2 = asyncio.create_task(second())
 
-    await asyncio.gather(task1, task2)
+async def get_files(url, session):
+    async with session.get(url, allow_redirects=True) as resp:
+        file = await resp.read()
+        write_files(file)
+
+
+async def main():
+    url = 'http://placekitten.com/200/300'
+    tasks = []
+
+    async with aiohttp.ClientSession() as session:
+        for _ in range(10):
+            task = asyncio.create_task(get_files(url, session))
+            tasks.append(task)
+
+        await asyncio.gather(*tasks)
 
 
 if __name__ == '__main__':
-    asyncio.run(loop())
-
+    start_time = time()
+    asyncio.run(main())
+    print(time()-start_time)

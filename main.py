44cdbe892bde2b146b3ce18f06461a4376cd3dc0
No-er <goglmogl.com@gmail.com>
2023-10-22 12:36:11 +0700

    asyncio lesson 7 part 1

--- main.py
+++ main.py
@@ -1,75 +1,25 @@
-import socket
-from select import select
+import asyncio
+from time import time
 
-tasks = []
-
-read_dic = {}
-write_dic = {}
-
-def server():
-    server_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_s.bind(("localhost", 5000))
-    server_s.listen()
-
+async def first():
+    num = 0
     while True:
+        print(num)
+        num += 1
+        await asyncio.sleep(1)
 
-        yield ('read', server_s)
-        client_s, addr = server_s.accept() #read
-
-        print('Connection from', addr)
-        tasks.append(client(client_s))
-
-
-
-def client(client_s):
+async def second():
     while True:
+        print(time())
+        await asyncio.sleep(1)
 
-        try:
-            yield ('read', client_s)
-            client_s.recv(4096) #read
+async def loop():
+    task1 = asyncio.create_task(first())
+    task2 = asyncio.create_task(second())
 
-            resp = "Hi MotherFucker\n".encode()
-
-            yield ('write', client_s)
-            client_s.send(resp) #write
-
-        except:
-            try:
-                tasks.remove(client_s)
-                client_s.close()
-            except:
-                pass
-
-
-
-
-def loop():
-    while any([tasks, read_dic, write_dic]):
-
-        while not tasks:
-            ready_4_read, ready_4_write, _ = select(read_dic, write_dic, [])
-
-            for sock in ready_4_read:
-                tasks.append(read_dic.pop(sock))
-            for sock in ready_4_write:
-                tasks.append(write_dic.pop(sock))
-
-        try:
-            task = tasks.pop(0)
-
-            mode, sock = next(task)
-
-            if mode == 'read':
-                read_dic[sock] = task
-            if mode == 'write':
-                write_dic[sock] = task
-        except StopIteration:
-            print('All Done')
-
+    await asyncio.gather(task1, task2)
 
 
 if __name__ == '__main__':
-    tasks.append(server())
-    loop()
+    asyncio.run(loop())
 
--- test.py
+++ test.py
@@ -1,5 +1,31 @@
-from sys import stdin
-print()
-print(type(stdin))
-765
+def dec(f):
+    def wrapper(*args, **kwargs):
+        g = f(*args, **kwargs)
+        g.send(None)
+        return g
+    return wrapper
 
+def gen():
+    x = 123
+    mes = yield x
+    print(mes)
+
+#@dec
+def ave():
+    sum = 0
+    count = 0
+    ave = 0
+
+    while True:
+        try:
+            print(1)
+            x = yield ave
+            print(2)
+        except:
+            pass
+        else:
+            count += 1
+            sum += x
+            ave = round(sum/count, 2)
+
+g = dec(ave)
